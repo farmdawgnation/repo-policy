55646857a8daeeb19af708203f847b36bc2ad5ca
Matt Farmer <matt@frmr.me>
2021-01-02 09:22:52 -0500

    Add picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,8 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    implementation("info.picocli:picocli:4.6.0")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     testImplementation("org.assertj:assertj-core:3.18.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
