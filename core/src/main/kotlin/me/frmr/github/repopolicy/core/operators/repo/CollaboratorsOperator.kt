22634b1ddaae9cf60fd9b9af8cb1d04e282c8da0
Matt Farmer <matt@frmr.me>
2021-01-03 22:19:43 -0500

    Collaborators operator

--- core/src/main/kotlin/me/frmr/github/repopolicy/core/operators/repo/CollaboratorsOperator.kt
+++ core/src/main/kotlin/me/frmr/github/repopolicy/core/operators/repo/CollaboratorsOperator.kt
@@ -6,10 +6,27 @@ import me.frmr.github.repopolicy.core.model.PolicyValidationResult
 import me.frmr.github.repopolicy.core.operators.NonEnforcingOperator
 import org.kohsuke.github.GHRepository
 
-class CollaboratorsOperator: NonEnforcingOperator() {
+class CollaboratorsOperator(desiredCollaborators: List<String>): NonEnforcingOperator() {
   override val description: String = "Collaborators"
+  private val desiredCollaboratorsSet = desiredCollaborators.toSet()
 
   override fun validate(target: GHRepository): PolicyValidationResult {
-    TODO("Not yet implemented")
+    val currentCollaboratorsSet = target.collaboratorNames
+
+    return if (currentCollaboratorsSet.containsAll(desiredCollaboratorsSet)) {
+      PolicyValidationResult(
+        subject = target.fullName,
+        description = "All desired collaborators are present",
+        passed = true
+      )
+    } else {
+      val missingCollaborators = desiredCollaboratorsSet - currentCollaboratorsSet
+      val descriptionStr = missingCollaborators.joinToString(", ")
+      PolicyValidationResult(
+        subject = target.fullName,
+        description = "The following collaborators are missing: $descriptionStr",
+        passed = false
+      )
+    }
   }
 }
