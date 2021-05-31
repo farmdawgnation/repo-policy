ede5df516fa499a4a7bee9802ea2eeff10faf06c
Matt Farmer <matt@frmr.me>
2021-05-31 08:23:12 -0400

    Branch protected root operator

--- core/src/main/kotlin/me/frmr/github/repopolicy/core/operators/branch/BranchProtectionRootOperator.kt
+++ core/src/main/kotlin/me/frmr/github/repopolicy/core/operators/branch/BranchProtectionRootOperator.kt
@@ -8,17 +8,51 @@ import org.kohsuke.github.GHRepository
 class BranchProtectionRootOperator(
   val branch: String,
   val enabled: Boolean,
-  val requireLinearHistory: Boolean?,
-  val allowForcePushes: Boolean?,
-  val allowDeletions: Boolean?
+  val requireLinearHistory: Boolean?, // TODO: listen to this value
+  val allowForcePushes: Boolean?, // TODO: listen to this value
+  val allowDeletions: Boolean? // TODO: listen to this value
 ): PolicyRuleOperator {
   override val description: String = "Branch protection"
 
   override fun validate(target: GHRepository): PolicyValidationResult {
-    TODO("Not yet implemented")
+    val branch = target.getBranch(branch)
+
+    if (branch == null) {
+      return PolicyValidationResult(
+        subject = target.fullName + "/" + branch,
+        description = "Branch does not exist",
+        passed = false
+      )
+    }
+
+    val isProtected = branch.isProtected
+    if (isProtected != enabled) {
+      return PolicyValidationResult(
+        subject = target.fullName + "/" + branch,
+        description = "Expected protection $enabled found protection $isProtected",
+        passed = false
+      )
+    }
+
+    return PolicyValidationResult(
+      subject = target.fullName + "/" + branch,
+      description = "Basic branch protection ok",
+      passed = true
+    )
   }
 
   override fun enforce(target: GHRepository): PolicyEnforcementResult {
-    TODO("Not yet implemented")
+    val validationResult = validate(target)
+
+    if (validationResult.passed) {
+
+    }
+
+    return PolicyEnforcementResult(
+      subject = target.fullName + "/" + branch,
+      description = "Enforced basic branch protection rules",
+      passedValidation = false,
+      policyEnforced = true
+    )
   }
 }
